4ca7072f8e296b7c5beace5c71534a32ac18858b
Divya <divya.shetty@revolutionit.com.au>
2020-09-23 15:48:36 +1000

    Updated the code so that default settings are not for Electron project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 AusPost-JavaTestAutomation with BDD
 
 Base java serinity framework with BDD, with web app and message tech stack support
+
+Update the chrome driver as per the Version in your System
--- src/test/java/com/auspost/runners/ShakeoutRunner.java
+++ src/test/java/com/auspost/runners/ShakeoutRunner.java
@@ -5,6 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "classpath:features", glue = "com.auspost", tags = "@type=electron")
+@CucumberOptions(features = "classpath:features", glue = "com.auspost", tags = "@type=cts")
 public class ShakeoutRunner {
 }
